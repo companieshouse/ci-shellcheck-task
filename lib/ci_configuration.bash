c2314b2188497553ff98524fc32149ac6b196cab
JoelClemence <jclemence@companieshouse.gov.uk>
2024-03-27 17:17:54 +0000

    Code review feedback

--- lib/ci_configuration.bash
+++ lib/ci_configuration.bash
@@ -3,7 +3,4 @@
 # This script is sourced and these variables are then used within the other
 # scripts
 # shellcheck disable=SC2034
-LOGS_REGEX='log/**'
-BACKUPS_REGEX='backups*/**'
-TESTS_REGEX='test/**'
 HIDDEN_FILE_OR_DIR_REGEX='*/[.]**'
